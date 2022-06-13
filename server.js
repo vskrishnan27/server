ce9add572ee2709acc95b9d75ac92b1123a50425
vskrishnan27 <vskrishnan27@gmail.com>
2022-06-13 22:30:37 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+// const express = data.json();
+
+const data = express();
+
+data.use(express.json())
+
+
+const port = process.env.port || 5000
+
+data.listen(port,()=>{
+    console.log(`http:localhost:${port}`)
+})
+
+data.get('/firsttry',(res,req)=>{
+    console.log(res);
+    req.status(200).send({
+        "status":"ok",
+        "name":"vskrishnan"
+    })
+})
+
+data.post('/firsttry/:id',(req,res)=>{
+
+        const {id} = req.params;
+        const {name} = req.body;
+
+        res.send({
+            'newname':'krishnan',
+            'id':`${id}`
+        })
+
+})
+
+// data.
