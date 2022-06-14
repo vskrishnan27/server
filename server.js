6d4f7f9d10cb051e4677b46caec3bcb4530a9ee2
vskrishnan27 <vskrishnan27@gmail.com>
2022-06-14 13:59:09 +0530

    added json

--- server.js
+++ server.js
@@ -11,9 +11,21 @@ const port = process.env.PORT || 5000
 data.listen(port,()=>{
     console.log(`http:localhost:${port}`)
 })
-
+const value = require('./json/MOCK_DATA.json')
 data.get('/',(req,res)=>{
-    res.send({"emptydata":"nodata"})
+    res.send(value)
+})
+
+data.post('/firsttry/:id',(req,res)=>{
+
+    const {id} = req.params;
+    const {name} = req.body;
+
+    res.send({
+        'newname':`'name`,
+        'id':`${id}`
+    })
+
 })
 
 data.get('/firsttry',(req,res)=>{
